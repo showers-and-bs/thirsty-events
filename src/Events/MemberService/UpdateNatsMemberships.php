a5c479d2a87653015ce8a9416f7b69caa701ee93
Predrag Rakonjac <predrag.rakonjac@quantox.com>
2023-09-18 14:31:40 +0200

    add UserRegistered and UpdateNatsMemberships

---
+++ src/Events/MemberService/UpdateNatsMemberships.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ShowersAndBs\ThirstyEvents\Events\MemberService;
+
+use ShowersAndBs\ThirstyEvents\Contracts\ShouldBePublished;
+
+class UpdateNatsMemberships implements ShouldBePublished
+{
+
+    /**
+     * @var int
+     */
+    public readonly int $userId;
+
+    /**
+     * @var string
+     */
+    public readonly string $username;
+
+    /**
+     * @var array
+     */
+    public readonly array $allChannels;
+
+    /**
+     * @var bool
+     */
+    public readonly bool $canDownload;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public static function createFromArray(array $userInfo)
+    {
+        $instance = new self;
+
+        $instance->userId      = $userInfo['userId'];
+        $instance->username    = $userInfo['username'];
+        $instance->allChannels = $userInfo['allChannels'];
+        $instance->canDownload = $userInfo['canDownload'];
+
+        return $instance;
+    }
+}
