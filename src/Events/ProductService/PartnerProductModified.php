36dc60bf235a099b24cf325a33e0dc76df695e50
Ivan Pantelic <158176587+ivanpantelic96@users.noreply.github.com>
2024-04-22 12:59:58 +0200

    THIR-691 - Partner product events (#26)

---
+++ src/Events/ProductService/PartnerProductModified.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ShowersAndBs\ThirstyEvents\Events\ProductService;
+
+use ShowersAndBs\ThirstyEvents\Contracts\ShouldBePublished;
+
+class PartnerProductModified implements ShouldBePublished
+{
+
+    /**
+     * @var string
+     */
+    public readonly int $partnerId;
+    
+    /**
+     * @var string
+     */
+    public readonly string $productId;
+
+    /**
+     * @var int|null
+     */
+    public readonly int|null $payoutPercentage;
+
+
+    /**
+     * Create a new event instance.
+     *
+     * @param int $partnerId
+     * @param string $productId
+     * @param int|null $payoutPercentage
+     */
+    public function __construct(
+        int  $partnerId,
+        string  $productId,
+        int|null  $payoutPercentage,
+    ) {
+        $this->partnerId = $partnerId;
+        $this->productId = $productId;
+        $this->payoutPercentage = $payoutPercentage;
+    }
+
+    /**
+     * Gets a string representation of the object
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return implode("\n", (array)$this);
+    }
+}
